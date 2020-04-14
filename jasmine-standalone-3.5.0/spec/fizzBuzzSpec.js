4b0678e187ad8c72a92e8beba972fe5f0b300608
Alex Lewis <alexlewis374@gmail.com>
2020-04-14 17:32:12 +0100

    first test working

--- jasmine-standalone-3.5.0/spec/fizzBuzzSpec.js
+++ jasmine-standalone-3.5.0/spec/fizzBuzzSpec.js
@@ -1,6 +1,6 @@
 describe('Fizzbuzz', function() {
   var fizzbuzz;
-  describe('knows when a number is', function() {
+  describe('know when a number is', function(){
     it('divisible by 3', function() {
       fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
@@ -9,4 +9,3 @@ describe('Fizzbuzz', function() {
 });
 
 
-/*describe('', function() {});*/
