7f743824a49cc079d2f425d49e1899cc3b8d2ff8
Graham <grahamalconer@icloud.com>
2020-04-14 16:55:29 +0100

    describe error

--- jasmine-standalone-3.5.0/spec/fizzBuzzSpec.js
+++ jasmine-standalone-3.5.0/spec/fizzBuzzSpec.js
@@ -1,14 +1,11 @@
 describe('Fizzbuzz', function() {
-  var fizzbuzz
+  var fizzbuzz;
   describe('knows when a number is', function() {
     it('divisible by 3', function() {
       fizzbuzz = new Fizzbuzz();
-      expect(isDivisibleByThree(3)).toBe(true);
-
+      expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
-
   });
-
 });
 
 
