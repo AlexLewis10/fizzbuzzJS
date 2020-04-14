ff9e1de0184ad491722000c6ce35f09a560ce69c
Alex Lewis <alexlewis374@gmail.com>
2020-04-14 17:54:45 +0100

    returns false if not divisible by 3

--- jasmine-standalone-3.5.0/spec/fizzBuzzSpec.js
+++ jasmine-standalone-3.5.0/spec/fizzBuzzSpec.js
@@ -1,11 +1,22 @@
 describe('Fizzbuzz', function() {
   var fizzbuzz;
+
+  beforeEach(function() {
+    fizzbuzz = new Fizzbuzz();
+  });
+
   describe('know when a number is', function(){
     it('divisible by 3', function() {
-      fizzbuzz = new Fizzbuzz();
-      expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
+    expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
   });
+
+  describe('knows when a number is NOT', function() {
+    it('divisible by 3', function() {
+    expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+    })
+  })
+
 });
 
 
